9af36019b86a17104c8d5c0bdb320add8f2c844d
leticialopez-aguilar <lopez.leticia.935@gmail.com>
2021-11-27 14:26:34 -0800

    Update Wheel.h

--- Wheel.h
+++ Wheel.h
@@ -1,6 +1,18 @@
 #ifndef WHEEL_H_INCLUDED
 #define WHEEL_H_INCLUDED
+using namespace std;
 
+class Wheel 
+{
+private:
+ string scores[25];
 
+public:
+  Wheel();
+  void setScores ();
+  string getRandomScore();
+  void displayScores();
+
+};
 
 #endif // WHEEL_H_INCLUDED
