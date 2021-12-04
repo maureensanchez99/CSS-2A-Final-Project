c436a83431b85e489c6c965838258adcdb493a4d
leticialopez-aguilar <lopez.leticia.935@gmail.com>
2021-12-03 17:12:15 -0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,6 +1,25 @@
 #ifndef PLAYER_H_INCLUDED
 #define PLAYER_H_INCLUDED
+using namespace std;
+#include <vector>
+#include <string>
 
+class Player 
+{
+private:
+  string name;
+  vector<int> totalScore;
+  char playerGuess;
+public:
+  Player();
+  Player(string name_);
+  void setName(string name_);
+  string getName() const;
+  void setPlayerGuess(char playerGuess_);
+  char getPLayerGuess() const;
+  void addToTotalScore(int score_);
+  void displayAddedScores();
 
+}
 
 #endif // PLAYER_H_INCLUDED
