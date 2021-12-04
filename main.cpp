b4d8915d10a5c77442ebb91bff398b825d6c2274
maureensanchez99 <39846411+maureensanchez99@users.noreply.github.com>
2021-12-04 12:59:43 -0800

    added in functions

--- main.cpp
+++ main.cpp
@@ -9,10 +9,21 @@
 
 using namespace std;
 
+void addPlayers(vector<Player> &players){
+    string userName = "";
+    for(int i = 0; i < 4; i++){
+        cout << "What is the name of Player " << i+1 << ": ";
+        cin >> userName;
+        players[i].setName(userName);
+    }
+}
+
 int main(){
-    vector<Player> players;//holds all player info
+    vector<Player> players(4);//holds all player info in vector as objects
+    Play gameStart;
 
-
+    addPlayers(players);//game starts out by getting players' names
+    gameStart.playGame();
 
     return 0;
 }
