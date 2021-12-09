91e7c123d6e80bce56130bbeb3473c434c88da3f
SudoHashira <95673123+SudoHashira@users.noreply.github.com>
2021-12-08 18:14:39 -0800

    Add files via upload

--- Phrase.cpp
+++ Phrase.cpp
@@ -1,4 +1,50 @@
-#include "Phrase.h"
 #include <iostream>
+#include <string>
+#include "Phrase.h"
+#include "Game.h"
+void Phrase::setCategory()
+{
+std::string category[] = {"Programming", "Popular Anime", "Salinas School", "Computer Science Pioneers", "Local Attractions"};
+}
 
+std::string Phrase::getCategory() const{
+int size;
+for(int i = 0; i < size; i++)
+{
+    return category[i];
+}
 
+void Phrase::setPhrase(){
+std::string phrase0[] = {"object oriented programming"};
+std::string phrase1[] = {"demon slayer"};
+std::string phrase2[] = {"hartnell community college"};
+std::string phrase3[] = {"ada lovelace"};
+std::string phrase4[] = {"monterey bay aquarium"};
+}
+
+std::string getPhrase() const
+{
+if(category[] == [0])
+{
+    return phrase0[];
+}
+else if(category[] == [1])
+{
+    return phrase1[];
+}
+else if(category[] == [2])
+{
+    return phrase2[];
+}
+else if(category[] == [3])
+{
+    return phrase3[];
+}
+else(category[] == [4])
+{
+    return phrase4[];
+}
+}
+
+}
+}
--- Phrase.h
+++ Phrase.h
@@ -1,9 +1,24 @@
-#ifndef Phrase_h
-#define Phrase_h
+#ifndef PHRASE_H_INCLUDED
+#define PHRASE_H_INCLUDED
+#include "Game.h"
 #include <iostream>
-
+#include <string>
+class Phrase
+{
 protected:
+std::string category; //provides a category as a clue to what the phrase might be
+std::string phrase; //phrase to be guessed by player
+char letterGuess; //letter guessed by player
 
 public:
+Phrase(){} //default constructor
+void setCategory();
+std::string getCategory() const;
+void setPhrase();
+std::string getPhrase() const;
 
-#endif /*Phrase_h*/
+};
+
+
+
+#endif // PHRASE_H_INCLUDED
